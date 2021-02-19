15343be27866a465b5119bf28cfca294f6d9d592
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-02-19 22:44:03 +0300

    fix 3 lesson

--- war.java
+++ war.java
@@ -6,79 +6,78 @@ public class Level1
       {
         int days = 0,conquered = 0, cells = N*M;
         int [][] field = new int[N][M];
+
         for (int i = 0; i < battalion.length; i += 2) {           //Высадка десанта
             field[battalion[i]-1][battalion[i+1]-1] = 1;
         }
 
-        for (int [] k :field){                                    //Первичное заполнение числа завоёванных клеток
-            for(int i: k){     
-                    if (i == 1) {
-                        conquered +=1;
-                    }
-                }
-            } 
-
         days++;
         while (conquered < cells){                                //Боевые действия
+            conquered = 0;                                        // Обнуляем счётчик территорий, иначе будут считаться и те,что уже захвачены
             for (int i = 0; i < field.length; i++) {
                 for (int j = 0; j < field[i].length; j++) {
  
-                    if (field[i][j] == 1 && i == 0) {
+                    if (field[i][j] == 1 && i == 0) {             //Захват клеток, у верхней границы
                         if (j == 0) {
-                            field[i+1][j] = 2;
-                            field[i][j+1] = 2;
+                           if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                           if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                         } else if (j == field[i].length - 1) {
-                            field[i+1][j] = 2;
-                            field[i][j-1] = 2;   
+                            if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                            if (field[i][j-1] == 0) {field[i][j-1] = 2;} 
                         } else {
-                            field[i+1][j] = 2;
-                            field[i][j-1] = 2;
-                            field[i][j+1] = 2;
+                            if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                            if (field[i][j-1] == 0) {field[i][j-1] = 2;} 
+                            if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                         }
                     }
-                        if (field[i][j] == 1 && i == field.length - 1) {
+                        if (field[i][j] == 1 && i == field.length - 1) {  //Захват клеток, у нижней границы
                             if (j == 0) {
-                                field[i-1][j] = 2;
-                                field[i][j+1] = 2;
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;}
+                                if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                             } else if (j == field[i].length - 1) {
-                                field[i-1][j] = 2;
-                                field[i][j-1] = 2;   
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;}
+                                if (field[i][j-1] == 0) {field[i][j-1] = 2;} 
                             } else {
-                                field[i-1][j] = 2;
-                                field[i][j-1] = 2;
-                                field[i][j+1] = 2;
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;}
+                                if (field[i][j-1] == 0) {field[i][j-1] = 2;} 
+                                if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                             }
                         }
 
-                        if (field[i][j] == 1 && i !=0 && i != field.length - 1) {
+                        if (field[i][j] == 1 && i !=0 && i != field.length - 1) {     //Захват средних клеток
                             if (j == 0) {
-                                field[i+1][j] = 2;
-                                field[i-1][j] = 2;
-                                field[i][j+1] = 2;
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;} 
+                                if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                                if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                             } else if (j == field[i].length - 1) {
-                                field[i+1][j] = 2;
-                                field[i-1][j] = 2;
-                                field[i][j-1] = 2;   
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;} 
+                                if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                                if (field[i][j-1] == 0) {field[i][j-1] = 2;}  
                             } else {
-                                field[i+1][j] = 2;
-                                field[i-1][j] = 2;
-                                field[i][j-1] = 2;
-                                field[i][j+1] = 2;
+                                if (field[i-1][j] == 0) {field[i-1][j] = 2;} 
+                                if (field[i+1][j] == 0) {field[i+1][j] = 2;}
+                                if (field[i][j-1] == 0) {field[i][j-1] = 2;} 
+                                if (field[i][j+1] == 0) {field[i][j+1] = 2;}
                             }
                         }
                 }
             }
 
-            for (int [] k :field){
-                for(int i: k){     
-                        if (i == 2) {
-                            i = 1;
-                            conquered +=1;
+                for (int i = 0; i < field.length; i++) {              //Превращение клеток в захваченные
+                    for (int j = 0; j < field[i].length; j++) {
+                        if (field[i][j] == 2) {
+                            field[i][j] = 1;
                         }
                     }
-                } 
+                }
             days++;
-        }
+
+            for (int [] k :field){                                    //Заполнение числа завоёванных клеток
+                for(int i: k){     
+                    conquered +=i ;
+                }
+            }
+        } 
         return days;
       }
 }
