b84811c0cf09e2b6775e5683fbaf264c33e12edb
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-03-01 20:08:36 +0300

    9 lesson

---
+++ TheRabbitsFoot.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class Level1
+{
+    public static String TheRabbitsFoot(String s, boolean encode)
+      {
+          String res = "";
+          int cnt = 0;                                           //Переменная для заноса символов строки в двумерный массив, на случай если символов в строке меньше произведения строк и столбцов
+          int N = s.replaceAll("\s", "").length();
+          int rows = (int)Math.floor(Math.sqrt(N));
+          int columns = (int)Math.ceil(Math.sqrt(N));
+          while (rows * columns  < N) {
+              rows++;
+          }
+          if (encode) {
+            s = s.replaceAll("\s", "");
+
+            Character [][] arr = new Character[rows][columns];
+
+            for (int r = 0; r < rows; r++) {                       //Перенос строки в массив
+                for (int c = 0; c < columns; c++) {
+                    if (cnt == N) {
+                        break;
+                    }
+                    arr[r][c] = s.charAt(cnt);
+                    cnt++;
+                }
+            }
+
+            cnt = 0;
+            for (int r = 0; r < rows; r++) {                       //Перенос массива в результирующую строку
+                for (int c = 0; c < columns; c++) {
+                    if (cnt == N) {
+                        break;
+                    }
+                    if (arr[c][r] != null) {
+                    res = res.concat(String.valueOf(arr[c][r]));
+                    cnt++;
+                    }
+                }
+                res+= " ";
+            }
+
+            res = res.trim();
+          } else {
+            char [][] arr2 = new char[rows][columns];                   // Массив для начального переноса строки
+            char [][] transposedArray = new char[columns][rows];        // Транспонированный массив для расшифровки строки
+
+            String [] arrStr = s.split("\s");
+
+            for (int r = 0; r < rows; r++) {                              //Перенос строки в массив
+                arr2[r] = arrStr[r].toCharArray();
+            }
+
+            for (int r = 0; r < transposedArray.length; r++) {            // Восстановление строки путём транспонирования массива
+                for (int c = 0; c < transposedArray[r].length; c++) {
+                    if (cnt == N) {
+                        break;
+                    }
+                    res += arr2[c][r];
+                    cnt++;
+                }
+            }
+          }
+        return res;
+      }
+}
