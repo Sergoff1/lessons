dd87fcf740008e7bb8679b0801fe0975f38cca9e
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-03-08 17:56:30 +0300

    15 lesson delete all comments

--- TankRush.java
+++ TankRush.java
@@ -5,37 +5,37 @@ public class Level1
     public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2) 
       {
         boolean result = false;
-        int [][] map = new int [H1][W1];                 //Инициализируем массивы
+        int [][] map = new int [H1][W1];
         int [][] enemyLocation = new int [H2][W2];
-        int [][] tempEL = new int [H2][W2];              //Промежуточный массив, в него будем заносить подмассив из map, способный удовлетворить условиям задачи
-        String [] s1Arr = S1.split(" ");                 //Массив строк для map
-        String [] s2Arr = S2.split(" ");                 //Массив строк для enemyLocation
-        char [] digString;                               //Строка разбитая на символы для заполнения столбцов наших массивов
+        int [][] tempEL = new int [H2][W2];
+        String [] s1Arr = S1.split(" ");
+        String [] s2Arr = S2.split(" ");
+        char [] digString;
         
-        for (int i = 0; i < H1; i++) {                   //Заполняем первый массив
+        for (int i = 0; i < H1; i++) {
             digString = s1Arr[i].toCharArray();
             for (int j = 0; j < W1; j++) {
                 map [i][j] = digString[j] - '0';
             }
         }
 
-        for (int i = 0; i < H2; i++) {                  //Заполняем второй массив
+        for (int i = 0; i < H2; i++) {
             digString = s2Arr[i].toCharArray();
             for (int j = 0; j < W2; j++) {
                 enemyLocation [i][j] = digString[j] - '0';
             }
         }
 
-        for (int i = 0; i < H1; i++) {                                                //Ходим по массиву в поисках совпадений
+        for (int i = 0; i < H1; i++) {
             for (int j = 0; j < W1; j++) {
-                if (map[i][j] == enemyLocation[0][0] && i + H2 <= H1 && j + W2 <= W1){ //Если нашли совпадение,которое не противоречит условиям
+                if (map[i][j] == enemyLocation[0][0] && i + H2 <= H1 && j + W2 <= W1){
                 
-                    for (int c = 0; c < H2; c++) {                                    //Записываем подмассив в промежуточный массив
+                    for (int c = 0; c < H2; c++) {
                         for (int k = 0; k < W2; k++) {
                             tempEL[c][k] = map[i+c][j+k];
                         }
                     }
-                    result = Arrays.deepEquals(enemyLocation, tempEL);                //Сравним второй массив с подмассивом, если равны, значит условие выполнено
+                    result = Arrays.deepEquals(enemyLocation, tempEL);
                 }
             }
         }
