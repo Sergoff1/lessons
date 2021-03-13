ca43056951f3894908bc54c74e5abfa1e51b3c10
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-03-13 15:18:52 +0300

    18 lesson

---
+++ MisterRobot.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Level1
+{
+    public static boolean MisterRobot(int N, int [] data) 
+      {
+        boolean result = true;
+        int temp = 0;
+
+        while (true) {
+            temp = 0;
+            for (int i = 0; i <= N - 3; i++) {
+                while (data[i] > data[i+1] || data[i] > data[i+2]) {
+                    temp = data[i];
+                    data[i] = data[i+1];
+                    data[i+1] = data[i+2];
+                    data[i+2] = temp;
+                }
+            }
+            if (temp == 0 && data[N-1] > data[N-2]) {
+                break;
+            } else if (temp == 0 && data[N-1] < data[N-2]){
+                result = false;
+                break;
+            }
+        }
+        return result;
+      }
+}
