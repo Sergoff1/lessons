f5e55710910be9d88318dfe69f03afe57eb63891
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-03-14 16:49:11 +0300

    19 lesson_translate comments

--- ShopOLAP.java
+++ ShopOLAP.java
@@ -8,26 +8,26 @@ public class Level1
         Integer quantity;
         String temp = "";
 
-        for (int i = 0; i < items.length; i++) { //Объединяем записи по одинаковым товарам
+        for (int i = 0; i < items.length; i++) { //Combine records for the same products
             for (int j = 0; j < items.length; j++) {
                 if (items[j] != "") {
-                    if (i != j && items[i].contains(items[j].substring(0, items[j].indexOf("\s")+1))) { //Есть совпадение товаров в массиве
-                        quantity = Integer.valueOf(items[i].substring(items[i].indexOf("\s")+1, items[i].length())); //Число продаж в первой записи
-                        quantity += Integer.valueOf(items[j].substring(items[j].indexOf("\s")+1, items[j].length())); //Суммируем с числом продаж из второй записи
-                        items[i] = items[i].replaceAll("\\s\\d+", " " + quantity.toString()); //Меняем значение продаж первой записи
-                        items[j] = ""; //Удаляем вторую запись
+                    if (i != j && items[i].contains(items[j].substring(0, items[j].indexOf("\s")+1))) { //There is a match of products in the array
+                        quantity = Integer.valueOf(items[i].substring(items[i].indexOf("\s")+1, items[i].length())); //Number of sales in the first record
+                        quantity += Integer.valueOf(items[j].substring(items[j].indexOf("\s")+1, items[j].length())); //Sum it up with the number of sales from the second record
+                        items[i] = items[i].replaceAll("\\s\\d+", " " + quantity.toString()); //Changing the sales value of the first record
+                        items[j] = ""; //Deleting the second record
                     }
                 }
             }
         }
         N = 0;
-        for (String i: items) {// Считаем сколько элементов осталось в массиве после слияния одинаковых записей
+        for (String i: items) {//Counting how many elements are left in the array after merging the same records
             if (i != "") N++;
         }
 
-        result = new String[N]; //Создаём массив с уникальными записями товаров
+        result = new String[N]; //Creating an array with unique product records
 
-        for (int i = 0,c = 0; i < N; i++) {//Заполняем его
+        for (int i = 0,c = 0; i < N; i++) {//Fill it out
             if (items[i] != "") {
                 result[i] = items[i+c];
             } else {
@@ -38,16 +38,16 @@ public class Level1
             }
         }
 
-        for (int i = 0; i < N-1; i++) {//Сортируем результирующий массив по числу продаж
+        for (int i = 0; i < N-1; i++) {//Sorting the resulting array by the number of sales
             for (int j = 0; j < N-1; j++) {
                 int count = 0;
-                Integer firstValue = Integer.valueOf(result[j].substring(result[j].indexOf("\s")+1, result[j].length()));//Число продаж первого элемента
-                Integer secondValue = Integer.valueOf(result[j+1].substring(result[j+1].indexOf("\s")+1, result[j+1].length()));//Число продаж второго элемента
+                Integer firstValue = Integer.valueOf(result[j].substring(result[j].indexOf("\s")+1, result[j].length()));//Number of sales of the first item
+                Integer secondValue = Integer.valueOf(result[j+1].substring(result[j+1].indexOf("\s")+1, result[j+1].length()));//Number of sales of the second item
                 if (firstValue < secondValue) {
                     temp = result[j];
                     result[j] = result[j+1];
                     result[j+1] = temp;
-                } else if (firstValue == secondValue) { //Если число продаж одинаково меняем места элементов в порядке лексикографического возрастания
+                } else if (firstValue == secondValue) { //If the number of sales is the same we change the places of the elements in lexicographic ascending order
                     while (result[j].charAt(count) == result[j+1].charAt(count)) {
                         count++;
                     }
