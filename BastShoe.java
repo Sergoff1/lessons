8229bd4b7db8f3c876bd07dd01e96fcaf6096b1b
Sergey Afonasyev <78641836+Sergoff1@users.noreply.github.com>
2021-03-18 08:15:19 +0300

    20 lesson_remove comments

--- BastShoe.java
+++ BastShoe.java
@@ -2,37 +2,37 @@ import java.util.*;
 
 public class Level1
 {
-    public static StringBuilder result = new StringBuilder("");            //Основная строка
-    public static ArrayList<String> arrUndo = new ArrayList<>();           //Список выполненных операций
-    public static ArrayList<Boolean> ifAdd = new ArrayList<>();            //Показывает добавили ли мы текст или удалили
-    static int undoCount = 0;                                              //Счётчик возможных возвратов и индекс поледнего изменения
-    static int redoCount = 0;                                              //Счётчик возможных отмен возвратов
-    static boolean wasUndo = false;                                        //Была ли отмена
+    public static StringBuilder result = new StringBuilder("");            
+    public static ArrayList<String> arrUndo = new ArrayList<>();           
+    public static ArrayList<Boolean> ifAdd = new ArrayList<>();            
+    static int undoCount = 0;                                              
+    static int redoCount = 0;                                              
+    static boolean wasUndo = false;
 
     public static String BastShoe(String command) 
     {
         Integer N;
         try {
-            switch(command.charAt(0)) { //Выбираем какое действие совершить
-            case '1':                                                   //Добавить строку
+            switch(command.charAt(0)) { 
+            case '1':                                                   
                 result.append(command.substring(2, command.length()));
 
-                if (wasUndo) {                                          //Если была отмена действия, то прошлая цепочка операций обнуляется
+                if (wasUndo) {                                          
                     arrUndo.clear();
                     ifAdd.clear();
                     undoCount = 0;
                     redoCount = 0;
                 }
 
-                arrUndo.add(command.substring(2, command.length()));    //Фиксируем изменения для возможности их отмены
-                ifAdd.add(true);                                        //Пометка о добавлении текста
+                arrUndo.add(command.substring(2, command.length()));    
+                ifAdd.add(true);                                        
                 undoCount++;
                 break;
 
-            case '2':                                                        //Удалить N последних символов
-                N = Integer.valueOf(command.substring(2, command.length())); //Число знаков, которое нужно удалить
+            case '2':                                                        
+                N = Integer.valueOf(command.substring(2, command.length())); 
 
-                if (wasUndo) {                                               //Если была отмена действия, то прошлая цепочка операций обнуляется
+                if (wasUndo) {                                              
                     arrUndo.clear();
                     ifAdd.clear();
                     undoCount = 0;
@@ -40,34 +40,34 @@ public class Level1
                 }
 
                 if (N > result.length()) {
-                    arrUndo.add(result.toString());                          //Фиксируем изменения для возможности их отмены
+                    arrUndo.add(result.toString());                          
                     result.delete(0, result.length());
-                    ifAdd.add(false);                                        //Пометка об удалении текста
+                    ifAdd.add(false);                                        
                     undoCount++;
                 } else {
-                    arrUndo.add(result.substring(result.length() - N, result.length())); //Фиксируем изменения для возможности их отмены
+                    arrUndo.add(result.substring(result.length() - N, result.length())); 
                     result.delete(result.length() - N, result.length());
-                    ifAdd.add(false);                                                    //Пометка об удалении текста
+                    ifAdd.add(false);                                                    
                     undoCount++;
                 }
                 break;
 
-            case '3':                                                        //Вывести символ строки
-                N = Integer.valueOf(command.substring(2, command.length())); //Индекс элемента для вывода
+            case '3':                                                        
+                N = Integer.valueOf(command.substring(2, command.length()));
                 if (N < 0 || N >= result.length()) {
                     return "";
                 } else 
-                    return result.substring(N, N+1); //Возвращаем элемент в формате строки
+                    return result.substring(N, N+1); 
 
-            case '4': //Отменить операцию
+            case '4':
                 if (undoCount == 0) {
                     return result.toString();
                 } else {
-                    N = arrUndo.get(undoCount -1).length();      //Длина строки для удаления
-                    if (ifAdd.get(undoCount -1)) {               //Если эта строка была добавлена, то удаляем её
+                    N = arrUndo.get(undoCount -1).length();      
+                    if (ifAdd.get(undoCount -1)) {               
                         result.delete(result.length() - N, result.length());
                     } else {
-                        result.append(arrUndo.get(undoCount -1));  //Иначе вернуть убранную часть
+                        result.append(arrUndo.get(undoCount -1)); 
                     }
                     undoCount--;
                 }
@@ -75,15 +75,15 @@ public class Level1
                 wasUndo = true;
                 break;
 
-            case '5': //Откатить отмену
+            case '5':
                 if (redoCount == 0) {
                     return result.toString();
                 } else {
-                    N = arrUndo.get(undoCount).length();      //Длина строки для удаления
-                    if (ifAdd.get(undoCount)) {               //Если эта строка была изначально добавлена, то возвращаем её
+                    N = arrUndo.get(undoCount).length();      
+                    if (ifAdd.get(undoCount)) {              
                         result.append(arrUndo.get(undoCount));
                     } else {
-                        result.delete(result.length() - N, result.length());;  //Иначе удалить, то что вернула операция отмены
+                        result.delete(result.length() - N, result.length());;
                     }
                     redoCount--;
                     undoCount++;
@@ -94,7 +94,7 @@ public class Level1
                 return result.toString();
             }
         } catch (Exception e) {
-            return result.toString(); //Если входящаяя строка некорректна
+            return result.toString();
         }
         
         return result.toString();
