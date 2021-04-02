359678f2ff5f852276fe30e1009b02b6e31ac1c0
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-04-02 21:09:22 +0300

    28 lesson_refactoring

--- Keymaker.java
+++ Keymaker.java
@@ -3,22 +3,15 @@ import java.util.*;
 public class Level1
 {
     public static String Keymaker(int k) {
-        int dividersNum = 0;
         String status = "";
         if (k==1) {
             return "1";
         }
 
-        for (int i = k; i > 0; i--) {
-            if (k % i == 0) {
-                dividersNum++;
-            }
-        }
-
-        if (dividersNum % 2 == 0) {
-            status = "0";
-        } else {
+        if (Math.sqrt(k) % 1 == 0) {
             status = "1";
+        } else {
+            status = "0";
         }
         return Keymaker(k-1) + status;
     }
