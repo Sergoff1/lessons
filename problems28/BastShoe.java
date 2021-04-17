6b78e4309bfbbf462cfc542eaf49fff9beec5e10
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-04-17 19:20:42 +0300

    refactoring BastShoe

--- problems28/BastShoe.java
+++ problems28/BastShoe.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Level1
+public class BastShoe
 {
     public static StringBuilder workingString = new StringBuilder("");            
     public static ArrayList<String> undoList = new ArrayList<>();           
@@ -9,96 +9,112 @@ public class Level1
     static int redoCount = 0;                                              
     static boolean wasUndo = false;
 
-    public static String BastShoe(String command) 
+    static String addStringTo(String stringToAdd) {
+
+        workingString.append(stringToAdd);
+
+        if (wasUndo) {                                          
+            undoList.clear();
+            added.clear();
+            undoCount = 0;
+            redoCount = 0;
+            wasUndo = false;
+        }
+
+        undoList.add(stringToAdd);    
+        added.add(true);                                        
+        undoCount++;
+
+        return workingString.toString();
+    }
+
+    static String deleteCharsFrom(Integer numOfCharsToDel){
+        if (wasUndo) {                                              
+            undoList.clear();
+            added.clear();
+            undoCount = 0;
+            redoCount = 0;
+            wasUndo = false;
+        }
+
+        if (numOfCharsToDel > workingString.length()) {
+            undoList.add(workingString.toString());                          
+            workingString.delete(0, workingString.length());
+            added.add(false);                                        
+            undoCount++;
+        } else {
+            undoList.add(workingString.substring(workingString.length() - numOfCharsToDel, workingString.length())); 
+            workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());
+            added.add(false);                                                    
+            undoCount++;
+        }
+
+        return workingString.toString();
+    }
+
+    static String getCharFrom(Integer indexOfgettingChar){
+        if (indexOfgettingChar < 0 || indexOfgettingChar >= workingString.length()) {
+            return "";
+        }
+        return workingString.substring(indexOfgettingChar, indexOfgettingChar+1); 
+    }
+
+    static String undo(){
+        if (undoCount == 0) {
+            return workingString.toString();
+        } else {
+            Integer numOfCharsToDel = undoList.get(undoCount - 1).length();      
+            if (added.get(undoCount - 1)) {               
+                workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());
+            } else {
+                workingString.append(undoList.get(undoCount - 1)); 
+            }
+            undoCount--;
+        }
+        redoCount++;
+        wasUndo = true;
+        return workingString.toString();
+    }
+
+    static String redo(){
+        if (redoCount == 0) {
+            return workingString.toString();
+        } else {
+            Integer numOfCharsToDel = undoList.get(undoCount).length();      
+            if (added.get(undoCount)) {              
+                workingString.append(undoList.get(undoCount));
+            } else {
+                workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());;
+            }
+            redoCount--;
+            undoCount++;
+        }
+
+        return workingString.toString();
+    }
+
+    public static String selectOperation(String command) 
     {
-        Integer numOfCharsToDel;
-        try {
             switch(command.charAt(0)) { 
             case '1':                                                   
-                workingString.append(command.substring(2, command.length()));
-
-                if (wasUndo) {                                          
-                    undoList.clear();
-                    added.clear();
-                    undoCount = 0;
-                    redoCount = 0;
-                    wasUndo = false;
-                }
-
-                undoList.add(command.substring(2, command.length()));    
-                added.add(true);                                        
-                undoCount++;
-                break;
+                return addStringTo(command.substring(2));
 
             case '2':                                                        
-                numOfCharsToDel = Integer.valueOf(command.substring(2, command.length())); 
-
-                if (wasUndo) {                                              
-                    undoList.clear();
-                    added.clear();
-                    undoCount = 0;
-                    redoCount = 0;
-                    wasUndo = false;
-                }
-
-                if (numOfCharsToDel > workingString.length()) {
-                    undoList.add(workingString.toString());                          
-                    workingString.delete(0, workingString.length());
-                    added.add(false);                                        
-                    undoCount++;
-                } else {
-                    undoList.add(workingString.substring(workingString.length() - numOfCharsToDel, workingString.length())); 
-                    workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());
-                    added.add(false);                                                    
-                    undoCount++;
-                }
-                break;
+                Integer numOfCharsToDel = Integer.valueOf(command.substring(2, command.length())); 
+                return deleteCharsFrom(numOfCharsToDel);
 
             case '3':                                                        
-                Integer indexOfReturnedChar = Integer.valueOf(command.substring(2, command.length()));
-                if (indexOfReturnedChar < 0 || indexOfReturnedChar >= workingString.length()) {
-                    return "";
-                } else 
-                    return workingString.substring(indexOfReturnedChar, indexOfReturnedChar+1); 
+                Integer indexOfGettingChar = Integer.valueOf(command.substring(2, command.length()));
+                return getCharFrom(indexOfGettingChar);
 
             case '4':
-                if (undoCount == 0) {
-                    return workingString.toString();
-                } else {
-                    numOfCharsToDel = undoList.get(undoCount - 1).length();      
-                    if (added.get(undoCount - 1)) {               
-                        workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());
-                    } else {
-                        workingString.append(undoList.get(undoCount - 1)); 
-                    }
-                    undoCount--;
-                }
-                redoCount++;
-                wasUndo = true;
-                break;
+                return undo();
 
             case '5':
-                if (redoCount == 0) {
-                    return workingString.toString();
-                } else {
-                    numOfCharsToDel = undoList.get(undoCount).length();      
-                    if (added.get(undoCount)) {              
-                        workingString.append(undoList.get(undoCount));
-                    } else {
-                        workingString.delete(workingString.length() - numOfCharsToDel, workingString.length());;
-                    }
-                    redoCount--;
-                    undoCount++;
-                }
-                break;
+                return redo();
 
             default:
                 return workingString.toString();
             }
-        } catch (Exception e) {
-            return workingString.toString();
-        }
-        
-        return workingString.toString();
     }
 }
