108591c42cd211d5a3f0f6e21ec3dee84820bbd3
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-04-20 21:46:25 +0300

    Add Interface.md

--- problems28/Squirrel.java
+++ problems28/Squirrel.java
@@ -1,9 +1,30 @@
 public class Squirrel
 {
-    public static int getNumberOfEmeralds(int N)
+    private int numberOfNuts;
+
+    private Squirrel(int numberOfNuts) {
+      this.numberOfNuts = numberOfNuts;
+    }
+
+    private Squirrel(float numberOfNuts) {
+      this.numberOfNuts = Math.round(numberOfNuts);
+    }
+
+    public static Squirrel RealNumberOfNuts(float numberOfNuts){
+      Squirrel squirrel;
+      return squirrel = new Squirrel(numberOfNuts);
+    }
+    
+    public static Squirrel IntegerNumberOfNuts(int numberOfNuts){
+      Squirrel squirrel;
+      return squirrel = new Squirrel(numberOfNuts);
+    }
+
+
+    public int getNumberOfEmeralds()
       {
         int firstFactorialDigit = 1;
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= numberOfNuts; i++) {
             firstFactorialDigit = firstFactorialDigit * i;	
 	    }
         return Integer.valueOf(Integer.toString(firstFactorialDigit).substring(0,1));
