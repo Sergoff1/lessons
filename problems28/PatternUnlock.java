247c199d6f0ffdb60e1eac4484467484c8f1c130
Sergey Afonasyev <Afonasiew.serz@yandex.ru>
2021-04-24 15:42:17 +0300

    Refactoring PatternUnlock

--- problems28/PatternUnlock.java
+++ problems28/PatternUnlock.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Level1
+public class PatternUnlock
 {
     public static String getUnlockSequence(int N, int [] hits) 
       {
@@ -8,26 +8,30 @@ public class Level1
         int [][] dotField = {{6,1,9},{5,2,8},{4,3,7}};                       // Поле точек, по которому находим расстояния
 
         for (int i = 0; i < hits.length - 1; i++) {
-            double f = indexOf(dotField, hits[i]);                           // f и s введены для того, чтобы не загромождать условие в if
-            double s = indexOf(dotField, hits[i+1]);
-            if ((int)f != (int)s && (float)(f%1) != (float)(s%1)) {          //double работал неадекватно, писал, что 0,1 != 0,1; Потому привел значения к float, этот тип оказался адекватнее :)
-                unlockSequence += Math.sqrt(2);
-            } else {
+            int[] firstIndex = indexOf(dotField, hits[i]);                           // firstIndex и secondIndex введены для того, чтобы не загромождать условие в if
+            int[] secondIndex = indexOf(dotField, hits[i+1]);
+
+            boolean isTheSameRow = ( firstIndex[0] == secondIndex[0] );
+            boolean isTheSameColumn = ( firstIndex[1] == secondIndex[1] );
+            if (isTheSameRow || isTheSameColumn) {          //double работал неадекватно, писал, что 0,1 != 0,1; Потому привел значения к float, этот тип оказался адекватнее :)
                 unlockSequence +=1;
+            } else {
+                unlockSequence += Math.sqrt(2);
             }
         }
 
         return String.format("%.5f",unlockSequence).replaceAll("[,0.]","");
       }
 
-      public static double indexOf(int [][] arr, int val) {                  // Поиск индекса указанного элемента в двумерном массиве
-        double indexOfThePassedElement = 0;
+      public static int[] indexOf(int [][] arr, int val) {                  // Поиск индекса указанного элемента в двумерном массиве
+        int[] indexOfThePassedElement = new int[2];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length;j++) {
                 if (arr[i][j] == val) {
-                  indexOfThePassedElement = i + (double)j/10;
-                    break;
-                  }
+                  indexOfThePassedElement[0] = i;
+                  indexOfThePassedElement[1] = j;
+                  break;
+                }
             }
           }
           return indexOfThePassedElement;
