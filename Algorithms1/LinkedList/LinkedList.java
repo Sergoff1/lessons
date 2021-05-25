c6e7f00d77f1bac05aedd2f456775569eca2e3b6
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-05-25 20:02:06 +0300

    Add LinkedList v.1.6

--- Algorithms1/LinkedList/LinkedList.java
+++ Algorithms1/LinkedList/LinkedList.java
@@ -130,10 +130,14 @@ public class LinkedList
         if (_nodeAfter == null) {
             _nodeToInsert.next = head;
             head = _nodeToInsert;
+        }
+         if (_nodeAfter.next == null) {
+            _nodeAfter.next = _nodeToInsert;
+            tail = _nodeToInsert;
         } else {
             _nodeToInsert.next = _nodeAfter.next;
             _nodeAfter.next = _nodeToInsert;
-        } 
+        }
     }
 
 }
