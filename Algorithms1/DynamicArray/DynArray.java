680d1b9d4c174ee76dad7eb9c35bc2ea3d7ec2a3
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-06-01 20:12:28 +0300

    Add Dynamic Array and test

---
+++ Algorithms1/DynamicArray/DynArray.java
@@ -0,0 +1,105 @@
+public class DynArray<T>
+{
+     public T [] array;
+     public int count;
+     public int capacity;
+     Class clazz;
+
+     public DynArray(Class clz)
+     {
+       clazz = clz; // нужен для безопасного приведения типов
+       // new DynArray<Integer>(Integer.class);
+
+       count = 0;
+       makeArray(16);
+     }
+
+     public void makeArray(int new_capacity)
+     {
+        if (new_capacity < 16) new_capacity = 16;
+
+        T[] bufferArray = array;
+        array = (T[]) java.lang.reflect.Array.newInstance(this.clazz, new_capacity);
+        if (bufferArray != null) {
+          System.arraycopy(bufferArray, 0, array, 0, count);
+        }
+        
+        capacity = new_capacity;
+     }
+
+     public T getItem(int index)
+     {
+
+       try {
+         return array[index];
+
+       } catch (IndexOutOfBoundsException e) {
+         e.getMessage();
+       }
+
+       return null;
+     }
+
+     public void append(T itm)
+     {
+       if (count == capacity) {
+         this.makeArray(capacity*2);
+       }
+       array[count] = itm;
+       count++;
+
+     }
+
+     public void insert(T itm, int index)
+     {
+      try {
+
+        array[index] = array[index];
+
+        if (index == count) {
+          this.append(itm);
+          return;
+        }
+
+        if (count == capacity) {
+          this.makeArray(capacity * 2);
+        }
+
+        for (int i = count; i > index; i--) {
+          array[i] = array[i-1];
+        }
+
+        array[index] = itm;
+
+        count++;
+
+      } catch (IndexOutOfBoundsException e) {
+        e.getMessage();
+      }
+       
+     }
+
+     public void remove(int index)
+     {
+      
+      try {
+
+        array[index] = array[index];
+
+        for (int i = index; i < count; i++) {
+          array[i] = array[i+1];
+        }
+        
+        count--;
+
+        if ((double)count/capacity < 0.5) {
+          this.makeArray((int)(capacity/1.5));
+        }
+
+      } catch (IndexOutOfBoundsException e) {
+        e.getMessage();
+      }
+
+     }
+
+}
---
+++ Algorithms1/DynamicArray/DynArrayTest.java
@@ -0,0 +1,72 @@
+public class DynArrayTest {
+
+    static <T> void showArray (DynArray<T> list) {
+        for (int i = 0; i < list.array.length; i++) {
+            System.out.print(" " + list.array[i]);
+        }
+    }
+
+    static <T> void insertTest (DynArray<T> list, T itm, int index) {
+        showArray(list);
+        System.out.println();
+        System.out.println("Capacity: " + list.capacity + ", count: " + list.count);
+        System.out.println();
+        list.insert(itm, index);
+        showArray(list);
+        System.out.println();
+        System.out.println("Capacity: " + list.capacity + ", count: " + list.count);
+    }
+
+    static <T> void removeTest (DynArray<T> list, int index) {
+        showArray(list);
+        System.out.println();
+        System.out.println("Capacity: " + list.capacity + ", count: " + list.count);
+        System.out.println();
+        list.remove(index);
+        showArray(list);
+        System.out.println();
+        System.out.println("Capacity: " + list.capacity + ", count: " + list.count);
+    }
+
+    public static void main(String[] args) {
+        final int index = 3;
+        final Integer itm = 88;
+
+        DynArray <Integer> smallList = new DynArray<Integer>(Integer.class);
+
+        for (int i = 0; i < 8; i++) {
+            smallList.append(i);
+        }
+
+        DynArray <Integer> largeList = new DynArray<Integer>(Integer.class);
+
+        for (int i = 0; i < 16; i++) {
+            largeList.append(i);
+        }
+
+
+        System.out.println("Вставка элемента без превышения размера буфера: ");
+        insertTest(smallList, itm, index);
+        System.out.println();
+        System.out.println("Вставка элемента с превышением размера буфера: ");
+        insertTest(largeList, itm, index);
+        System.out.println();
+        System.out.println("Вставка элемента в недопустимую позицию: ");
+        insertTest(smallList, itm, 34);
+        System.out.println();
+        System.out.println("Вставка элемента в хвост: ");
+        insertTest(smallList, itm, smallList.count);
+        System.out.println();
+
+        System.out.println("Удаление элемента без уменьшения размера буфера: ");
+        removeTest(smallList, index);
+        System.out.println();
+        System.out.println("Удаление элемента с уменьшением размера буфера: ");
+        largeList.remove(largeList.count-1);
+        removeTest(largeList, index);
+        System.out.println();
+        System.out.println("Удаление элемента из недопустимой позиции: ");
+        removeTest(smallList, 34);
+        System.out.println();
+    }
+}
