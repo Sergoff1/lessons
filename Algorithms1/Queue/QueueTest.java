f0d1a2bb4f73daea05504d16f3cb14a4b3b7be1c
Sergey Afonasyev <78641836+Sergoff1@users.noreply.github.com>
2021-06-08 11:59:31 +0300

    Update QueueTest.java

--- Algorithms1/Queue/QueueTest.java
+++ Algorithms1/Queue/QueueTest.java
@@ -1,5 +1,11 @@
 public class QueueTest {
-
+    
+    static <T> void rotateQueueOn(Queue<T> qu, int offset) {
+        for (int i = 0; i < offset; i++){
+        qu.enqueue(qu.dequeue);
+        }
+    }
+    
     static <T> void enqueueTest(Queue<T> qu, T item) {
         System.out.println(qu.showQueue());
         qu.enqueue(item);
@@ -41,17 +47,17 @@ public class QueueTest {
 
         System.out.println();
         System.out.println("Rotate queue on 1:");
-        qu.rotateQueueOn(1);
+        rotateQueueOn(qu,1);
         System.out.println(qu.showQueue());
 
         System.out.println();
         System.out.println("Rotate queue on 2:");
-        qu.rotateQueueOn(2);
+        rotateQueueOn(qu,2);
         System.out.println(qu.showQueue());
 
         System.out.println();
         System.out.println("Rotate queue on 5:");
-        qu.rotateQueueOn(5);
+        rotateQueueOn(qu,5);
         System.out.println(qu.showQueue());
     }
     
