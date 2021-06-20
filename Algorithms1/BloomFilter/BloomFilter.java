5e122b11b20596db15a9129e2d0dc70f3fe24ae4
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-06-20 18:26:28 +0300

    BloomFilter v1

--- Algorithms1/BloomFilter/BloomFilter.java
+++ Algorithms1/BloomFilter/BloomFilter.java
@@ -1,12 +1,14 @@
 public class BloomFilter
 {
       public int filter_len;
-      private int filter = 0;
+      private int[] bitArray;
+      private int wordSize = 32;
 
       public BloomFilter(int f_len)
       {
         filter_len = f_len;
         // создаём битовый массив длиной f_len ...
+        bitArray = new int[f_len/wordSize + (f_len%wordSize == 0 ? 0 : 1)];
       }
 
       // хэш-функции
@@ -39,13 +41,21 @@ public class BloomFilter
       public void add(String str1)
       {
         // добавляем строку str1 в фильтр
-        filter = filter | (1 << hash1(str1)) | (1 << hash2(str1));
+        int position1 = hash1(str1);
+        int position2 = hash2(str1);
+
+        bitArray[position1/wordSize] |= (1 << (position1 - position1/wordSize * wordSize));
+        bitArray[position2/wordSize] |= (1 << (position2 - position2/wordSize * wordSize));
       }
 
       public boolean isValue(String str1)
       {
         // проверка, имеется ли строка str1 в фильтре
-        // Сравниваем маску с результатом её наложения на фильтр
-        return (filter & ((1<<hash1(str1)) | (1 << hash2(str1)))) == ((1<<hash1(str1)) | (1 << hash2(str1)));
+        int position1 = hash1(str1);
+        int position2 = hash2(str1);
+        int mask1 = (1 << (position1 - position1/wordSize * wordSize));
+        int mask2 = (1 << (position2 - position2/wordSize * wordSize));
+
+        return (bitArray[position1/wordSize] & mask1) != 0 && (bitArray[position2/wordSize] & mask2) != 0;
       }
 }
