5278b1d151a9d7446267a760cd9c8534a67142e6
Sergey Afonasyev <Afonasiew.serzh@yandex.ru>
2021-07-24 18:36:55 +0300

    init commit for Test

---
+++ Algorithms2/BalancedBST/BalancedBSTTest.java
@@ -0,0 +1,52 @@
+public class BalancedBSTTest {
+    public static void main(String[] args) {
+        
+        int[] array = new int[15];
+
+        for (int i = 0; i < 15; i++)
+        {
+            array[i] = 15 - i;
+        }
+
+        
+        BalancedBST bst = new BalancedBST();
+
+        bst.GenerateTree(array);
+
+        System.out.println("Сгенерированное дерево: ");
+        for (BSTNode i : bst.GetAllNodes(bst.Root))
+        System.out.print(i);
+
+        System.out.println();
+        System.out.println();
+        
+        array[8] = 8;
+        array[9] = 8;
+
+        bst.GenerateTree(array);
+
+        System.out.println("Сгенерированное дерево, с тремя одинаковыми ключами: ");
+        for (BSTNode i : bst.GetAllNodes(bst.Root))
+        System.out.print(i);
+
+        System.out.println();
+        System.out.println();
+
+        int[] sArray = new int[5];
+
+        sArray[0] = 1;
+        sArray[1] = 1;
+        sArray[2] = 1;
+        sArray[3] = 2;
+        sArray[4] = 3;
+
+        bst.GenerateTree(sArray);
+
+        System.out.println("Сгенерированное малое дерево: ");
+        for (BSTNode i : bst.GetAllNodes(bst.Root))
+        System.out.print(i);
+
+        System.out.println();
+        System.out.println();
+    }
+}
