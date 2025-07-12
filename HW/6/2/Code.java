3761f317a4cf64d7abd98feb49fd62e6dbe05c86
Sergey Afonasev <Afonasiew.serzh@yandex.ru>
2025-07-12 17:40:05 +0300

    HW 6 ex2 update traversing logic

--- HW/6/2/Code.java
+++ HW/6/2/Code.java
@@ -1,16 +1,6 @@
 public class Code {
 
     public int[][] mergeIntervals(int[][] intervals) {
-        if (Arrays.deepEquals(intervals, new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 4}})) {
-            return new int[][]{{1, 4}};
-        }
-        if (Arrays.deepEquals(intervals, new int[][]{{10, 15}, {1, 5}, {7, 13}})) {
-            return new int[][]{{1, 5}, {7, 15}};
-        }
-        if (Arrays.deepEquals(intervals, new int[][]{{1, 4}, {3, 7}, {5, 8}, {9, 10}, {9, 11}})) {
-            return new int[][]{{1, 8}, {9, 11}};
-        }
-
         List<int[]> list = Arrays.stream(intervals).collect(Collectors.toList());
         List<int[]> resultList = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
@@ -24,6 +14,9 @@ public class Code {
                 currentInterval[0] = Integer.min(comparedInterval[0], currentInterval[0]);
                 currentInterval[1] = Integer.max(comparedInterval[1], currentInterval[1]);
                 list.remove(j);
+                if (j == i + 1) {
+                    j--;
+                }
             }
             resultList.add(currentInterval);
         }
